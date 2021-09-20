f5627b68b0f60dad2b802e05d1f578346cc70c26
nk-akun <nk_akun@163.com>
2021-09-20 16:58:25 +0800

    feat: 优化build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,16 @@
-if [-d "output"]; then
+if [ -d "output" ]; then
     rm -r output
 fi
 
-if [-d "auto_clock_in"]; then
+if [ -f "auto_clock_in" ]; then
     rm auto_clock_in
 fi
 
 go build -o auto_clock_in
 mkdir output
-if [-d "auto_clock_in"]; then
+if [ -f "auto_clock_in" ]; then
     mv auto_clock_in output
-fi
-
-if [-d "auto_clock_in"]; then
-    rm auto_clock_in
+    echo "编译成功!"
 else
     echo "编译失败!"
 fi
