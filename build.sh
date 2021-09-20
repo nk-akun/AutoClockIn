84aec6d4aca4e2ee3e4503e0bad1501f7ece921d
nk-akun <nk_akun@163.com>
2021-09-20 16:52:40 +0800

    feat: 优化build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
+if [-d "output"]; then
+    rm -r output
+fi
+
+if [-d "auto_clock_in"]; then
+    rm auto_clock_in
+fi
+
 go build -o auto_clock_in
 mkdir output
-mv auto_clock_in output
+if [-d "auto_clock_in"]; then
+    mv auto_clock_in output
+fi
+
+if [-d "auto_clock_in"]; then
+    rm auto_clock_in
+else
+    echo "编译失败!"
+fi
